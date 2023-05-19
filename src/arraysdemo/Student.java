5a0b5cde06186292dff367b1365da46d34155358
Hevin Vadodariya <vadodahe@sheridancollege.ca>
2023-05-19 13:03:19 -0400

    first commit

---
+++ src/arraysdemo/Student.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student;
+
+
+public class Student {
+   private String sId;
+   private String sName;
+
+    public Student(String sId, String sName) {
+        this.sId = sId;
+        this.sName = sName;
+    }
+
+   
+    public String getsId() {
+        return sId;
+    }
+
+    public void setsId(String sId) {
+        this.sId = sId;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+   
+   
+   
+   
+    
+       
+    }
+    
+}
